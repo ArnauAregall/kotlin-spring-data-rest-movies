dfbc692a170b50c260605cf917a3f6b5d782fedb
Arnau Aregall <arnau.are@gmail.com>
2023-03-28 16:17:03 +0200

    [Test] Use @Nested tests

--- src/test/kotlin/tech/aaregall/lab/movies/repository/rest/DirectorRestRepositoryIT.kt
+++ src/test/kotlin/tech/aaregall/lab/movies/repository/rest/DirectorRestRepositoryIT.kt
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType.APPLICATION_JSON
@@ -27,107 +28,127 @@ class DirectorRestRepositoryIT(
     @Autowired val objectMapper: ObjectMapper,
     @Autowired val directorRestRepository: DirectorRestRepository) : AbstractIT() {
 
-    @Test
-    fun `Should GET Directors paginated` () {
-        val directors = IntStream.rangeClosed(1, 50)
-            .mapToObj {index -> directorRestRepository.save(
-                Director( "FirstName $index", "LastName $index")
-            )}
-            .toList()
+    @Nested
+    inner class GET {
 
-        mockMvc.perform(get(BASE_PATH)
-            .accept(APPLICATION_JSON)
-            .param("page", "0")
-            .param("size", directors.size.toString()))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
-            .andExpectAll(
-                jsonPath("$._embedded.directors.length()").value(directors.size),
-                jsonPath("$._embedded.directors[*].id",
-                    containsInAnyOrder(streamToIsMatcher(directors.stream().map{ it.id.toInt() }))),
-                jsonPath("$._embedded.directors[*].first_name",
-                    containsInAnyOrder(streamToIsMatcher(directors.stream().map(Director::firstName)))),
-                jsonPath("$._embedded.directors[*].last_name",
-                    containsInAnyOrder(streamToIsMatcher(directors.stream().map(Director::lastName)))),
-                jsonPath("$.page").isNotEmpty,
-                jsonPath("$.page.size").value(directors.size),
-                jsonPath("$.page.total_elements").value(directors.size),
-                jsonPath("$.page.total_pages").value(1),
-                jsonPath("$.page.number").value(0)
-            )
+        @Test
+        fun `Should GET Directors paginated` () {
+            val directors = IntStream.rangeClosed(1, 50)
+                .mapToObj {index -> directorRestRepository.save(
+                    Director( "FirstName $index", "LastName $index")
+                )}
+                .toList()
+
+            mockMvc.perform(get(BASE_PATH)
+                .accept(APPLICATION_JSON)
+                .param("page", "0")
+                .param("size", directors.size.toString()))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpectAll(
+                    jsonPath("$._embedded.directors.length()").value(directors.size),
+                    jsonPath("$._embedded.directors[*].id",
+                        containsInAnyOrder(streamToIsMatcher(directors.stream().map{ it.id.toInt() }))),
+                    jsonPath("$._embedded.directors[*].first_name",
+                        containsInAnyOrder(streamToIsMatcher(directors.stream().map(Director::firstName)))),
+                    jsonPath("$._embedded.directors[*].last_name",
+                        containsInAnyOrder(streamToIsMatcher(directors.stream().map(Director::lastName)))),
+                    jsonPath("$.page").isNotEmpty,
+                    jsonPath("$.page.size").value(directors.size),
+                    jsonPath("$.page.total_elements").value(directors.size),
+                    jsonPath("$.page.total_pages").value(1),
+                    jsonPath("$.page.number").value(0)
+                )
+        }
+
+        @Test
+        fun `Should GET a Director by ID` () {
+            val director = directorRestRepository.save(Director("Terence", "Young"))
+
+            mockMvc.perform(get("${BASE_PATH}/${director.id}")
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpectAll(
+                    jsonPath("$.id").value(director.id.toInt()),
+                    jsonPath("$.first_name").value(director.firstName),
+                    jsonPath("$.last_name").value(director.lastName),
+                    jsonPath("$._links").isNotEmpty,
+                    jsonPath("$._links.self.href", containsString("${BASE_PATH}/${director.id}")),
+                    jsonPath("$._links.director.href", containsString("${BASE_PATH}/${director.id}")),
+                    jsonPath("$._links.movies.href", containsString("${BASE_PATH}/${director.id}/movies")),
+                )
+        }
+
     }
 
-    @Test
-    fun `Should GET a Director by ID` () {
-        val director = directorRestRepository.save(Director("Terence", "Young"))
+    @Nested
+    inner class POST {
 
-        mockMvc.perform(get("${BASE_PATH}/${director.id}")
-            .accept(APPLICATION_JSON))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
-            .andExpectAll(
-                jsonPath("$.id").value(director.id.toInt()),
-                jsonPath("$.first_name").value(director.firstName),
-                jsonPath("$.last_name").value(director.lastName),
-                jsonPath("$._links").isNotEmpty,
-                jsonPath("$._links.self.href", containsString("${BASE_PATH}/${director.id}")),
-                jsonPath("$._links.director.href", containsString("${BASE_PATH}/${director.id}")),
-                jsonPath("$._links.movies.href", containsString("${BASE_PATH}/${director.id}/movies")),
-            )
+        @Test
+        fun `Should Create a Director` () {
+            val director = Director("Terence", "Young")
+
+            val result = mockMvc.perform(post(BASE_PATH)
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(director)))
+                .andExpect(status().isCreated)
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpectAll(
+                    jsonPath("$.id").value(notNullValue()),
+                    jsonPath("$.first_name").value(director.firstName),
+                    jsonPath("$.last_name").value(director.lastName)
+                )
+                .andReturn()
+
+            val directorId = JsonPath.read<Int>(result.response.contentAsString, "$.id")
+
+            assertThat(directorRestRepository.getReferenceById(directorId.toLong()))
+                .isNotNull
+                .extracting(Director::firstName, Director::lastName)
+                .containsExactly(director.firstName, director.lastName)
+        }
+
     }
 
-    @Test
-    fun `Should POST a Director` () {
-        val director = Director("Terence", "Young")
+    @Nested
+    inner class PATCH {
 
-        val result = mockMvc.perform(post(BASE_PATH)
-            .accept(APPLICATION_JSON)
-            .contentType(APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(director)))
-            .andExpect(status().isCreated)
-            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
-            .andExpectAll(
-                jsonPath("$.id").value(notNullValue()),
-                jsonPath("$.first_name").value(director.firstName),
-                jsonPath("$.last_name").value(director.lastName)
-            )
-            .andReturn()
+        @Test
+        fun `Should PATCH a Director` () {
+            val director = directorRestRepository.save(Director("Martine", "Campbelly"))
 
-        val directorId = JsonPath.read<Int>(result.response.contentAsString, "$.id")
-
-        assertThat(directorRestRepository.getReferenceById(directorId.toLong()))
-            .isNotNull
-            .extracting(Director::firstName, Director::lastName)
-            .containsExactly(director.firstName, director.lastName)
-    }
-
-    @Test
-    fun `Should PATCH a Director` () {
-        val director = directorRestRepository.save(Director("Martine", "Campbelly"))
-
-        mockMvc.perform(patch("${BASE_PATH}/${director.id}")
-            .accept(APPLICATION_JSON)
-            .contentType(APPLICATION_JSON)
-            .content("""
+            mockMvc.perform(patch("${BASE_PATH}/${director.id}")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .content("""
                 {"first_name": "Martin", "last_name": "Campbell"}
                 """.trimIndent()))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
-            .andExpectAll(
-                jsonPath("$.id").value(director.id.toInt()),
-                jsonPath("$.first_name").value("Martin"),
-                jsonPath("$.last_name").value("Campbell")
-            )
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpectAll(
+                    jsonPath("$.id").value(director.id.toInt()),
+                    jsonPath("$.first_name").value("Martin"),
+                    jsonPath("$.last_name").value("Campbell")
+                )
+        }
+
     }
 
-    @Test
-    fun `Should DELETE a Director` () {
-        val director = directorRestRepository.save(Director("John", "Glen"))
+    @Nested
+    inner class DELETE {
 
-        mockMvc.perform(delete("${BASE_PATH}/${director.id}"))
-            .andExpect(status().isNoContent)
+        @Test
+        fun `Should DELETE a Director` () {
+            val director = directorRestRepository.save(Director("John", "Glen"))
 
-        assertThat(directorRestRepository.findById(director.id)).isEmpty
+            mockMvc.perform(delete("${BASE_PATH}/${director.id}"))
+                .andExpect(status().isNoContent)
+
+            assertThat(directorRestRepository.findById(director.id)).isEmpty
+        }
+
     }
 
 }
