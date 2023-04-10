bfcacd4c13da7ac67a151b797124910bec744b03
Arnau Aregall <arnau.are@gmail.com>
2023-04-10 12:07:18 +0200

    Set ON DELETE CASCADE to jointables

--- src/main/resources/db/migration/V0.0.1__create_schema.sql
+++ src/main/resources/db/migration/V0.0.1__create_schema.sql
@@ -28,21 +28,23 @@ CREATE TABLE IF NOT EXISTS "movie" (
     release_date DATE NOT NULL,
     director_id BIGSERIAL NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (director_id) REFERENCES director(id)
+    FOREIGN KEY (director_id) REFERENCES director(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "movie_character" (
     movie_id BIGSERIAL NOT NULL,
     character_id BIGSERIAL NOT NULL,
     PRIMARY KEY (movie_id, character_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(id),
-    FOREIGN KEY (character_id) REFERENCES character(id)
+    UNIQUE (movie_id, character_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (character_id) REFERENCES character(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "actor_character" (
     actor_id BIGSERIAL NOT NULL,
     character_id BIGSERIAL NOT NULL,
     PRIMARY KEY (actor_id, character_id),
-    FOREIGN KEY (actor_id) REFERENCES actor(id),
-    FOREIGN KEY (character_id) REFERENCES character(id)
+    UNIQUE (actor_id, character_id),
+    FOREIGN KEY (actor_id) REFERENCES actor(id) ON DELETE CASCADE,
+    FOREIGN KEY (character_id) REFERENCES character(id) ON DELETE CASCADE
 );
